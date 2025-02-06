7cc9f2524376ed602999f3b0238553db972519ef
sertanrdn <sertanerdogan07@gmail.com>
2025-02-06 19:39:33 +0100

    Updated app.js to be the latest version from main branch

--- src/app.js
+++ src/app.js
@@ -1,38 +1,7 @@
-<<<<<<< HEAD
-import { fetchPopularFilms } from "./api.js";
-import { renderFilms } from "./views/renderFilm.js";
-import { showError } from "./views/errorHandling.js";
-
-async function loadApp() {
-    const appDiv = document.getElementById('app');
-
-    // Show a loading state while fetching data
-    const spinner = document.createElement('div');
-    spinner.classList.add('spinner');
-    appDiv.appendChild(spinner);
-
-    try {
-        const films = await fetchPopularFilms();
-
-        if (films && films.results) {
-            renderFilms(films);
-        } else {
-            console.error('No film found!');
-        }
-        
-    } catch(error) {
-        console.error('Error loading films', error);
-        showError('Failed to load films. Please try again later.');
-    } finally {
-        // Remove the spinner once the data is loaded or an error occurs
-        appDiv.removeChild(spinner);
-    }
-=======
 import { initHomePage } from "./pages/home.js";
 
 async function loadApp() {
     await initHomePage();
->>>>>>> main
 }
 
 window.addEventListener('load', loadApp);
